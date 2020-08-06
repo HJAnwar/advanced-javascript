f5ac7bfb50a40d0b891a7eb9bcc5d892f976bfdd
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-08-06 12:10:33 -0700

    now

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,65 @@
+const first = 0;
+const second = false;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+const first = 1;
+const second = false;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+const first = 0;
+const second = true;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+const first = 1;
+const second = true;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+const first = 0;
+const second = 9;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+const first = 12;
+const second = 12;
+
+if(first == second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
