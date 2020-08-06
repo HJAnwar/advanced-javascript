d6fdec0b981f80b7027345eb7e85e0df2447b9c6
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-08-06 11:52:45 -0700

    added

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add (num1 ,num2){
+//      console.log(num1 + num2);
+    
+// }
+// const result = add(12, 23);
+// console.log(result);
+
+// function add (num1 ,num2){
+//     console.log(num1 ,num2);
+// }
+// const result1  = add(12);
+// console.log(result1);
+
+// const friend = {name:"samiya", phone:01919282837};
+// console.log(friend.gf);
+
+// let  fun = undefined;
+// console.log(fun);
+
+let ages = [12, 14, 32, 52];
+console.log(ages[12]);
+
