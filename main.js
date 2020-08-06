8f95053cf18fedd777962997ec168265ffcc52a6
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-08-06 11:28:03 -0700

    now added

---
+++ main.js
@@ -0,0 +1,99 @@
+// false Element
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// truthy Element
+
+// " "
+// "0"
+// []
+// {}
+
+
+
+
+let name  = false;
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = 0;
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = NaN;
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = null;
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  =undefined;
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = '';
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+// truthy element
+
+let name  = {"false"};
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = ["false"];
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+let name  = "0";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
